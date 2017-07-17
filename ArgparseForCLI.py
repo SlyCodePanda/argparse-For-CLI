ab309bd0301723dca180cdb8d4f3df6c82c5a406
Renee <reneemarsland@hotmail.com>
2017-07-17 20:07:21 +0930

    second commit

---
+++ ArgparseForCLI.py
@@ -0,0 +1,48 @@
+##def calc( x, y, operation ):
+##    if operation == 'add':
+##        return x + y
+##    elif operation == 'sub':
+##        return x - y
+##    elif operation == 'mul':
+##        return x * y
+##    elif operation == 'div':
+##        return x / y
+##
+##operation = calc( 7, 3, 'div' )
+##print( operation )
+
+# We are going to convert the above into a command-line interface using argparse.
+
+import argparse
+import sys
+
+def main():
+    # 'parser' is a object of the 'ArgumentParser' class.
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--x', type=float, default=1.0,
+                        help='What is the first number?')
+    parser.add_argument('--y', type=float, default=1.0,
+                        help='What is the second number?')
+    parser.add_argument('--operation', type=str, default='add',
+                        help='What opperation? (add, sub, mul, or div)')
+    
+    args = parser.parse_args()
+    # So the output will come to the console, we do the following:
+    sys.stdout.write(str(calc(args)))
+
+
+def calc( args ):
+    if args.operation == 'add':
+        return args.x + args.y
+    elif args.operation == 'sub':
+        return args.x - args.y
+    elif args.operation == 'mul':
+        return args.x * args.y
+    elif args.operation == 'div':
+        return args.x / args.y
+
+
+if __name__ == '__main__':
+    main()
+
